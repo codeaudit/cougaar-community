28a23b6a78bd989adb3fc9e644691d20cfeb59c1
ahelsing <ahelsing>
2003-02-13 23:26:15 +0000

    Bug 1611: Decide which CommunityInitializerService to use based on what the node was configured with: If the Node used the CSMART DB, then use that. Otherwise, use the File (XML) version

--- community/src/org/cougaar/community/init/CommunityInitializerServiceComponent.java
+++ community/src/org/cougaar/community/init/CommunityInitializerServiceComponent.java
@@ -27,11 +27,15 @@ import org.cougaar.core.component.ServiceProvider;
 import org.cougaar.core.component.ServiceBroker;
 import org.cougaar.core.node.DBInitializerService;
 import org.cougaar.core.node.NodeControlService;
+import org.cougaar.core.node.Node;
+import org.cougaar.core.service.LoggingService;
 import org.cougaar.util.GenericStateModelAdapter;
 
 /** 
  * A component which creates and advertises the appropriate 
  * CommunityInitializerService ServiceProvider.
+ * It can initialize from the CSMART database, using the <code>DBInitializerService</code>,
+ * or from XML files, depending on where components were intialized from.
  * <p>
  * @see FileCommunityInitializerServiceProvider
  * @see DBCommunityInitializerServiceProvider
@@ -44,6 +48,7 @@ implements Component
 
   private DBInitializerService dbInit;
   private ServiceProvider theSP;
+  private LoggingService log;
 
   public void setBindingSite(BindingSite bs) {
     // this.sb = bs.getServiceBroker();
@@ -59,26 +64,68 @@ implements Component
 
   public void load() {
     super.load();
+
+    log = (LoggingService)
+      sb.getService(this, LoggingService.class, null);
+    if (log == null) {
+      log = LoggingService.NULL;
+    }
+
+
+    // Do not provide this service if there is already one there.
+    // This allows someone to provide their own component to provide
+    // the community initializer service in their configuration
+    if (sb.hasService(CommunityInitializerService.class)) {
+      // already have CommunityInitializer service!
+      //
+      // leave the existing service in place
+      if (log.isInfoEnabled()) {
+        log.info(
+            "Not loading the default community initializer service");
+      }
+      if (log != LoggingService.NULL) {
+        sb.releaseService(this, LoggingService.class, log);
+        log = null;
+      }
+      return;
+    }
+
     theSP = chooseSP();
-    sb.addService(CommunityInitializerService.class, theSP);
+    if (theSP != null)
+      sb.addService(CommunityInitializerService.class, theSP);
+
+    if (log != LoggingService.NULL) {
+      sb.releaseService(this, LoggingService.class, log);
+      log = null;
+    }
   }
 
   public void unload() {
-    sb.revokeService(CommunityInitializerService.class, theSP);
+    if (theSP != null) {
+      sb.revokeService(CommunityInitializerService.class, theSP);
+      theSP = null;
+    }
     super.unload();
   }
 
   private ServiceProvider chooseSP() {
     try {
       ServiceProvider sp;
-      if (dbInit == null) {
-        sp = new FileCommunityInitializerServiceProvider();
-      } else {
+      String prop = System.getProperty(Node.INITIALIZER_PROP);
+      if (prop != null && prop.indexOf("DB") != -1 && dbInit != null) {
         sp = new DBCommunityInitializerServiceProvider(dbInit);
+	if (log.isInfoEnabled())
+	  log.info("Using CSMART DB CommunityInitializer");
+      } else {
+	// Note that these files are XML
+        sp = new FileCommunityInitializerServiceProvider();
+	if (log.isInfoEnabled())
+	  log.info("Using File (XML) CommunityInitializer");
       }
       return sp;
     } catch (Exception e) {
-      throw new RuntimeException("Exception while creating "+getClass().getName(), e);
+      log.error("Exception creating CommunityInitializerService", e);
+      return null;
     }
   }
 }
