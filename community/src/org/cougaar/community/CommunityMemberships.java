e4642a7c7112dd70681179b67dbf15498f9dad45
rsnyder <rsnyder>
2003-04-09 19:38:23 +0000

    Refactored out of CommunityPlugin

---
+++ community/src/org/cougaar/community/CommunityMemberships.java
@@ -0,0 +1,35 @@
+/*
+ * <copyright>
+ *  Copyright 2001-2003 Mobile Intelligence Corp
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+package org.cougaar.community;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ * Identifies communities that this agent is a member of.  This map is
+ * compared to Community objects received from the various community
+ * managers sending Community data to this agent.  If a mis-match is detected
+ * the agent will attempt to rectify by sending an join or leave request
+ * to manager.  A mis-match is likely the result of a community manager
+ * termination/restart.
+ */
+class CommunityMemberships extends HashMap implements Serializable {
+}
