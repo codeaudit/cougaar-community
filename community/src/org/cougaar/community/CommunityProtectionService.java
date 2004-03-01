286e55437b66d146d6bc04e356e95093f759e450
rsnyder <rsnyder>
2004-03-01 14:18:52 +0000

    Initial specification of interface for plugging in access control capability.

---
+++ community/src/org/cougaar/community/CommunityProtectionService.java
@@ -0,0 +1,48 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 Mobile Intelligence Corp
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+//package org.cougaar.core.service.community;
+package org.cougaar.community;
+
+import org.cougaar.core.component.Service;
+
+/**
+ */
+
+public interface CommunityProtectionService extends Service {
+
+  /**
+   * Authorize request to read or modify community state.
+   * @param communityName String  Name of affected community
+   * @param requester String      Name of requesting agent
+   * @param operation int         Requested operation (refer to
+   *                         org.cougaar.core.service.CommunityServiceConstants
+   *                              for valid op codes)
+   * @param target String         Name of affected community member or null if
+   *                              target is community
+   * @return boolean              Return true if request is authorized by
+   *                              current policy
+   */
+  boolean authorize(String communityName,
+                    String requester,
+                    int    operation,
+                    String target);
+
+}
