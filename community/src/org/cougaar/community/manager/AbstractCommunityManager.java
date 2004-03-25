d4f03ca99d3e9deda40d163394e101634185039b
rsnyder <rsnyder>
2004-03-25 12:23:38 +0000

    Added argument for revised CommunityProtectionService.authorize method.

--- community/src/org/cougaar/community/manager/AbstractCommunityManager.java
+++ community/src/org/cougaar/community/manager/AbstractCommunityManager.java
@@ -1,14 +1,14 @@
 /*
  * <copyright>
- *  
+ *
  *  Copyright 2001-2004 Mobile Intelligence Corp
  *  under sponsorship of the Defense Advanced Research Projects
  *  Agency (DARPA).
- * 
+ *
  *  You can redistribute this software and/or modify it under the
  *  terms of the Cougaar Open Source License as published on the
  *  Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -20,7 +20,7 @@
  *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  * </copyright>
  */
 package org.cougaar.community.manager;
@@ -113,7 +113,8 @@ public abstract class AbstractCommunityManager
                                  source,
                                  reqType,
                                  entity != null ? entity.getName()
-                                               : communityName)) {
+                                                : communityName,
+                                 attrMods)) {
       if (logger.isWarnEnabled()) {
         logger.warn(agentName + ": Authorization Failure:" +
                                 " community=" + communityName +
